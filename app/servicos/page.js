b10717435de595811921f2ba061826c587b37268
Larissa Coutinho <160404240+LarissaCoutinhoo@users.noreply.github.com>
2025-06-09 21:28:41 -0300

    .
    .

---
+++ app/servicos/page.js
@@ -0,0 +1,16 @@
+export default function Servicos() {
+  return (
+    <div>
+      <h1>Serviços</h1>
+       <br></br>
+       <br></br>
+      <h2>O que oferecemos 🎧</h2>
+       <br></br>
+      <ul>
+        <li>Streaming ilimitado</li>
+        <li>Compartilhe suas playlists favoritas</li>
+        <li>Descobertas personalizadas baseada no seu gosto</li>
+      </ul>
+    </div>
+  )
+}
---
+++ app/sobre/page.js
@@ -0,0 +1,8 @@
+export default function Sobre() {
+  return (
+    <div>
+      <h1>Sobre</h1>
+      <p>Movidos pela paixão pela música que conecta pessoas e transforma momentos!</p>
+    </div>
+  )
+}
